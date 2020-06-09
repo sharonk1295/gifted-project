cff9d2157bf8496bfda3cffac2500f6e81615bed
Sharon Kim <carolinek@SKs-MacBook-Air.local>
2020-06-09 14:48:32 -0400

    changed import on index.js

--- gifted_frontend/src/index.js
+++ gifted_frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "../src/components/App.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
