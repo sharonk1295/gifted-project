a996e293175c79c44f41ca379cfdf11cda1a02da
Sheila Kelley <spkelley26@gmail.com>
2020-06-09 10:34:11 -0700

    Added controllers

--- gifted_project_api/controllers/gifts.js
+++ gifted_project_api/controllers/gifts.js
@@ -14,7 +14,37 @@ router.post('/', async (req, res) => {
 })
 
 //Get
-router.get('/')
+router.get('/', async (req, res) => {
+    try{
+        const gifts = await Gift.find({})
+        res.status(200).json(gifts)
+    }catch(error){
+        res.status(400).json(error)
+    }
+})
+
+//Delete
+router.delete('/:id', async (req, res) => {
+    try{
+        const deletedGift = await Gift.findByIdAndRemove(req.params.id)
+        res.status(200).json(deletedGift)
+    }catch(error){
+        res.status(400).json(error)
+    }
+})
+
+//Update
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedGift = await Gift.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true}
+        )
+    }catch(error) {
+        res.status(400).json(error)
+    }
+})
 
 
 module.exports = router
